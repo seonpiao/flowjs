6de953663693cb45221e913d0d0d4a01c23c1d47
seon <seon.piao@gmail.com>
2013-05-20 18:23:29 +0800

    0.1.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 define(function(require,exports,module){
     window.Flowjs = {
-        V:'1.2.11',
+        V:'0.1.0',
         Class:require('./util/class'),
         Flow:require('./flow'),
         Step:require('./step'),

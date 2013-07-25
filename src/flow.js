b942ea4851ac324285aa3057ad61fb5ebc3174f9
seon <seon.piao@gmail.com>
2013-07-25 22:01:39 +0800

    0.4.0

--- src/flow.js
+++ src/flow.js
@@ -96,14 +96,14 @@ define(function(require,exports,module){
                         }
                         this.__timer = setTimeout(function(){
                             //执行到此，说明一个流程链已经完成，当前步骤为该流程链的末端，不允许再有下一步了
-                            _this.__newflow();
+                            step.end();
                             _this.__queue.clear();
                         },0);
                     }
                     else{
                         this.__timer = setTimeout(function(){
                             //执行到此，说明一个流程链已经完成，当前步骤为该流程链的末端，不允许再有下一步了
-                            _this.__newflow();
+                            step.end();
                             _this.__start();
                             _this.__queue.clear();
                         },0);
@@ -112,7 +112,9 @@ define(function(require,exports,module){
                 //未实现的步骤直接跳过
                 else{
                     this.__timer = setTimeout(function(){
-                        _this.__newflow();
+                        if (_this.__prev) {
+                            _this.__prev.end();
+                        }
                         _this.__start();
                         _this.__queue.clear();
                     },0);
